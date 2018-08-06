90a8faf6c0af3b5a59d8660dfa9e2094cb99bc6d
pedroffkpm <pedroffkamp@gmail.com>
2018-08-06 17:38:19 -0300

    port mistake

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.prepare()
       return handle(req, res)
     })
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
